64baad24f1ae6f0bbfca1dfe1e587de90eebf711
ciphercs <invicible2010@gmail.com>
2023-05-21 19:46:09 +1000

    Initial commit

---
+++ src/Driver.java
@@ -0,0 +1,27 @@
+package src;
+
+import src.utility.GameCallback;
+import src.utility.PropertiesLoader;
+import matachi.mapeditor.editor.Controller;
+
+import java.util.Properties;
+
+public class Driver {
+    public static final String DEFAULT_PROPERTIES_PATH = "properties/test1.properties";
+
+    /**
+     * Starting point
+     * @param args the command line arguments
+     */
+
+    public static void main(String args[]) {
+        String propertiesPath = DEFAULT_PROPERTIES_PATH;
+        if (args.length > 0) {
+            propertiesPath = args[0];
+        }
+        final Properties properties = PropertiesLoader.loadPropertiesFile(propertiesPath);
+        GameCallback gameCallback = new GameCallback();
+        new Controller();
+//        new src.Game(gameCallback, properties);
+    }
+}
---
+++ src/PacManGameGridAdapter.java
@@ -0,0 +1,60 @@
+package src;
+
+import ch.aplu.jgamegrid.Location;
+
+public class PacManGameGridAdapter extends src.PacManGameGrid
+{
+  private int nbHorzCells;
+  private int nbVertCells;
+  private int[][] mazeArray;
+
+  public PacManGameGridAdapter(int nbHorzCells, int nbVertCells, String mapString)
+  {
+    super(nbHorzCells, nbVertCells);
+    this.nbHorzCells = nbHorzCells;
+    this.nbVertCells = nbVertCells;
+    mazeArray = new int[nbVertCells][nbHorzCells];
+
+    // Copy structure into integer array
+    for (int i = 0; i < nbVertCells; i++)
+    {
+      for (int k = 0; k < nbHorzCells; k++) {
+        int value = toInt(mapString.charAt(nbHorzCells * i + k));
+        mazeArray[i][k] = value;
+      }
+    }
+  }
+
+  public int getCell(Location location)
+  {
+    return mazeArray[location.y][location.x];
+  }
+  private int toInt(char c)
+  {
+    if (c == 'b') // wall
+      return 0;
+    if (c == 'c') // pill
+      return 1;
+    if (c == 'a')
+      return 2;
+    if (c == 'f')
+      return 2;
+    if (c == 'g')
+      return 2;
+    if (c == 'h')
+      return 2;
+    if (c == 'i')
+      return 2;
+    if (c == 'j')
+      return 2;
+    if (c == 'k')
+      return 2;
+    if (c == 'l')
+      return 2;
+    if (c == 'd') // gold
+      return 3;
+    if (c == 'e') // ice
+      return 4;
+    return -1;
+  }
+}
